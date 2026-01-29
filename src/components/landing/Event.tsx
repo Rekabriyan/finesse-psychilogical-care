ac3e5b78ed203615262439b118831e357e99db55
Rekabriyan <rekabriyanch08@gmail.com>
2026-01-29 16:53:54 +0700

    feat: add Event component for landing page

---
+++ src/components/landing/Event.tsx
@@ -0,0 +1,14 @@
+
+
+const Event = () => {
+  return (
+    <section
+      id="event"
+      className="py-20 lg:py-32 bg-background relative overflow-hidden"
+    >
+      <div className="container mx-auto px-4 lg:px-8" ref={ref}></div>
+    </section>
+  );
+};
+
+export default Event;
